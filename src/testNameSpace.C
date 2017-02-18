5b1f1358db44144ee50bce9c79db8280ed79d150
John Campbell <johnmarcampbell@gmail.com>
2017-02-18 13:05:24 -0500

    Add basic azifem namespace

---
+++ src/testNameSpace.C
@@ -0,0 +1,10 @@
+#include "azifemNamespace_runId.cxx"
+// #include "../src/azifem_namespace.cxx"
+void testNameSpace()
+{
+    Int_t a = 3;
+    cout << azifem::AuAuRunIds[azifem::nAuAuRuns-1] << endl;
+    cout << azifem::nAuAuRuns << endl;
+    // cout << azifem::UURunIds[3] << endl;
+
+}
