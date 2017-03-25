0c485627dfd83fb9f70175cfdaa43d19804ce923
John Campbell <johnmarcampbell@gmail.com>
2017-03-25 14:00:27 -0400

    Update scripts/params file for new submitter version

--- submit/qaSubmission.py
+++ submit/qaSubmission.py
@@ -1,11 +1,12 @@
-from starsubmit.starsubmit import *
+from starsubmit.job import Job
+from starsubmit.request import Request
 from subprocess import call
 from argparse import ArgumentParser
 
 
 def cli_parser():
     parser = ArgumentParser('cli_parser')
-    parser.add_argument('paramater_file', help='File that contains job paramaters')
+    parser.add_argument('parameter_file', help='File that contains job parameters')
     parser.add_argument('xml_file', help='Create an xml file with this name')
     parser.add_argument('-s', action='store_true', 
                         help='Submit the xml file after creating')
@@ -14,8 +15,15 @@ def cli_parser():
 if __name__ == '__main__':
     args = cli_parser().parse_args()
 
-    sub = Submission(args.paramater_file)
-    sub.make_xml()
-    sub.write_xml(args.xml_file)
+    job = Job(args.parameter_file)
+    request = Request(job)
+
+    with open(args.xml_file, 'w') as f:
+        f.write(request.__str__())
+
     if args.s:
         call(['star-submit', args.xml_file])
+
+
+#     if args.s:
+#         call(['star-submit', args.xml_file])
